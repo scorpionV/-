bc024c6b01e00b4188172a29a18268d415e83500
652133064@qq.com <65213364@qq.com>
2021-08-15 23:27:03 +0800

    3.27

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './App'
 import router from './router'
+// import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
 new Vue({
-    el: '#app',
-    router,
-    render: h =>h(App)
+	el: '#app',
+	router,
+
+	template: '<App/>',
+	components: { App }
 })
